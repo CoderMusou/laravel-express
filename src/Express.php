768ceb87a67673d51ceafe61dee3445eb4ddd19a
widuu <admin@widuu.com>
2016-09-20 22:35:20 +0800

    update

--- src/Express.php
+++ src/Express.php
@@ -5,11 +5,13 @@ namespace widuu\Express;
 class Express
 {
 
-	public function __construct(){
+	public function __construct()
+    {
 
 	}
 
-	public function update(){
+	public function update()
+    {
 
 		$content = file_get_contents("http://m.kuaidi100.com/all/");
 
@@ -34,4 +36,9 @@ class  ExpressName{
 EOF;
     $c = file_put_contents('/Volumes/HDDONE/wwwroot/laravel/a.php', $string);
 	}
+
+    public function getCompany($number)
+    {
+
+    }
 }
